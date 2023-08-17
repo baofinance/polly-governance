18ebc4d818d7c8986b784f01ffa9acfb301e7f09
Haruxe <haruxe@proton.me>
2023-08-17 01:06:08 -0700

    what the hell did I even do

--- script/01_Deploy.s.sol
+++ script/01_Deploy.s.sol
@@ -4,28 +4,20 @@ pragma solidity ^0.8.13;
 import "../lib/forge-std/src/Script.sol";
 import "../src/PollyGovernor.sol";
 import "../src/ERC1967Proxy.sol";
-import "../src/VPolly.sol";
 import "../lib/openzeppelin-contracts/contracts/governance/utils/IVotes.sol";
 import "../lib/openzeppelin-contracts/contracts/governance/TimelockController.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract Deploy is Script {
 
     PollyGovernor public pollyGovernor;
     TimelockController public timelockController;
     address public pollyToken;
-    VPolly public vPolly;
-
-    address public testnetPolly = 0xC0adF78641409093cfc59D4b88b8c83394803489;
-    address public mainnetPolly = 0x4C392822D4bE8494B798cEA17B43d48B2308109C;
+    IVotes public vePolly = IVotes(0xCbAEC82448DAE09418bCd78Bf7654CB9De5a9D8d);
 
     function run() public {
         vm.startBroadcast();
-
-        pollyToken = 0xC0adF78641409093cfc59D4b88b8c83394803489;
-
-        // Deploy VPolly
-        vPolly = new VPolly(IERC20(pollyToken));
-        console2.log("VPolly deployed at:", address(vPolly));
 
         address[] memory proposers = new address[](1);
         proposers[0] = address(this);
@@ -35,7 +27,7 @@ contract Deploy is Script {
         console2.log("Timelock Controller deployed at:", address(timelockController));
 
         // Deploy Governor
-        pollyGovernor = new PollyGovernor(vPolly, timelockController);
+        pollyGovernor = new PollyGovernor(vePolly, timelockController);
         console2.log("Governor deployed at:", address(pollyGovernor));
 
         vm.stopBroadcast();
